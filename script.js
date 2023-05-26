f02f8ede9cdb9c97b1de07bed628a0d3f0d4927d
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-26 16:34:25 -0300

    decimo requisito completo e lint corrigido

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const loginBtn = document.getElementById("login-btn");
-const inputEmail = document.getElementById("input-email");
-const inputPw = document.getElementById("input-pw");
+const loginBtn = document.getElementById('login-btn');
+const inputEmail = document.getElementById('input-email');
+const inputPw = document.getElementById('input-pw');
 
 // Função do requisito 3: valida valores digitados no header ao clicar no botão Entrar.
-loginBtn.addEventListener("click", () => {
+loginBtn.addEventListener('click', () => {
   if (inputEmail.value && inputPw.value) {
-    alert("Olá, Tryber!");
+    alert('Olá, Tryber!');
   } else {
-    alert("Email ou senha inválidos.");
+    alert('Email ou senha inválidos.');
   }
 });
