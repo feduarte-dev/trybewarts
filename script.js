34ff3bcea420b492af3279776b62406d54c17d4f
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-26 15:33:46 -0300

    terceiro requisito completo

---
+++ script.js
@@ -0,0 +1,12 @@
+const loginBtn = document.getElementById("login-btn");
+const inputEmail = document.getElementById("input-email");
+const inputPw = document.getElementById("input-pw");
+
+// Função que valida valores digitados no header ao clicar no botão Entrar.
+loginBtn.addEventListener("click", () => {
+  if (inputEmail.value && inputPw.value) {
+    alert("Olá, Tryber!");
+  } else {
+    alert("Email ou senha inválidos.");
+  }
+});
