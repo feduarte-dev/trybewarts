f0a94f6cb7c2b0c0fa46bdc4796d29a79e6498db
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-26 20:13:33 -0300

    linter checked

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const loginBtn = document.getElementById("login-btn");
-const inputEmail = document.getElementById("email-input");
-const inputPw = document.getElementById("pw-input");
-const submitBtn = document.getElementById("submit-btn");
-const checkbox = document.getElementById("agreement");
+const loginBtn = document.getElementById('login-btn');
+const inputEmail = document.getElementById('email-input');
+const inputPw = document.getElementById('pw-input');
+const submitBtn = document.getElementById('submit-btn');
+const checkbox = document.getElementById('agreement');
 
 // Função do requisito 3: valida valores digitados no header ao clicar no botão Entrar.
-loginBtn.addEventListener("click", () => {
+loginBtn.addEventListener('click', () => {
   if (inputEmail.value && inputPw.value) {
-    alert("Olá, Tryber!");
+    alert('Olá, Tryber!');
   } else {
-    alert("Email ou senha inválidos.");
+    alert('Email ou senha inválidos.');
   }
 });
 
 // Pegamos a idéia a partir desse link: https://stackoverflow.com/questions/18110865/how-to-disable-enable-a-button-with-a-checkbox-if-checked
-checkbox.onchange = function () {
+checkbox.onchange = function habilitaBotao() {
   if (checkbox.checked) {
     submitBtn.disabled = false;
   } else {
